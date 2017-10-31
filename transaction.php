6443a30700e7074060f0798d0712706e08451d5e
Priyanshu Verma <priyanshuverma036@gmail.com>
2017-10-31 17:47:44 +0530

    Transactions
    files for transaction issue

---
+++ transaction.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Transaction</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+        <style type="text/css">
+            a:link{
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <center>
+            <h1>Mode Of Transactions</h1><br><br>
+                <button class="btn btn-default"><a href="deposit_transaction.php">DEPOSIT</a></button><br><br>
+                <button class="btn btn-default"><a href="withdrawl_transaction.php">WITHDRAWL</a></button>
+            </center>
+        </div>
+    </body>
+</html>
